023d819b0f4ff30d685c23b24ae7db8056af1e05
alexanderdoroshenko <ale.doroshenko@gmail.com>
2021-11-23 16:26:42 +0100

    sync from oryx: swap arrows layer button

--- keyboards/moonlander/keymaps/oryxbranch/keymap.c
+++ keyboards/moonlander/keymaps/oryxbranch/keymap.c
@@ -62,8 +62,8 @@ enum tap_dance_codes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
     KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           TD(DANCE_0),                                    TD(DANCE_2),    KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           MO(5),                                          LALT(LGUI(LCTL(LSFT(KC_T)))),KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
-    MO(2),          KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           MO(4),                                                                          KC_MEH,         KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      LT(2,KC_QUOTE),
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           MO(4),                                          LALT(LGUI(LCTL(LSFT(KC_T)))),KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
+    MO(2),          KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           MO(5),                                                                          KC_MEH,         KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      LT(2,KC_QUOTE),
     KC_LSHIFT,      LCTL_T(KC_Z),   KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         RCTL_T(KC_SLASH),KC_RSHIFT,
     LT(2,KC_GRAVE), WEBUSB_PAIR,    MO(4),          MO(3),          TD(DANCE_1),    LALT_T(KC_APPLICATION),                                                                                                KC_ESCAPE,      ST_MACRO_0,     TG(1),          KC_LBRACKET,    KC_RBRACKET,    KC_TRANSPARENT,
     KC_SPACE,       KC_BSPACE,      KC_LGUI,                        TG(1),          KC_RGUI,        KC_ENTER
