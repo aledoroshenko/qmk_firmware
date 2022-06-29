b94ca1b7ce31068eaec428a96bdca7917470e173
alexanderdoroshenko <ale.doroshenko@gmail.com>
2022-06-29 15:47:34 +0200

    sync from oryx: modal russian layout, not toggle

--- keyboards/moonlander/keymaps/oryxbranch/keymap.c
+++ keyboards/moonlander/keymaps/oryxbranch/keymap.c
@@ -68,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           MO(4),                                          LALT(LGUI(LCTL(LSFT(KC_T)))),KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
     MO(2),          KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           MO(5),                                                                          KC_MEH,         KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      LT(2,KC_QUOTE),
     KC_LSHIFT,      MT(MOD_LCTL, KC_Z),KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         MT(MOD_RCTL, KC_SLASH),KC_RSHIFT,
-    LT(2,KC_GRAVE), WEBUSB_PAIR,    MO(4),          MO(3),          TD(DANCE_1),    MT(MOD_LALT, KC_APPLICATION),                                                                                                KC_ESCAPE,      ST_MACRO_0,     TG(1),          KC_LBRACKET,    KC_RBRACKET,    KC_TRANSPARENT,
-    KC_SPACE,       KC_BSPACE,      KC_LGUI,                        TG(1),          KC_RGUI,        KC_ENTER
+    LT(2,KC_GRAVE), WEBUSB_PAIR,    MO(4),          MO(3),          TD(DANCE_1),    MT(MOD_LALT, KC_APPLICATION),                                                                                                KC_ESCAPE,      ST_MACRO_0,     TO(1),          KC_LBRACKET,    KC_RBRACKET,    KC_TRANSPARENT,
+    KC_SPACE,       KC_BSPACE,      KC_LGUI,                        MT(MOD_LALT, KC_LGUI),KC_RGUI,        KC_ENTER
   ),
   [1] = LAYOUT_moonlander(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
