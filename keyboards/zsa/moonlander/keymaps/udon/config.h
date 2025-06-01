88e51b5b5a2451f4105cf19a9bfda5d136611239
alexanderdoroshenko <ale.doroshenko@gmail.com>
2025-06-01 12:05:25 +0200

    Add combos for Mouseless

--- keyboards/zsa/moonlander/keymaps/udon/config.h
+++ keyboards/zsa/moonlander/keymaps/udon/config.h
@@ -1,12 +1,7 @@
-/*
-  Set any config.h overrides for your specific keymap here.
-  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
-*/
-
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "QmMmD/VlJxd"
+#define SERIAL_NUMBER "QmMmD/0WKOZw"
 #define LAYER_STATE_8BIT
-#define COMBO_COUNT 1
+#define COMBO_COUNT 3
 
 #define RGB_MATRIX_STARTUP_SPD 60
 
