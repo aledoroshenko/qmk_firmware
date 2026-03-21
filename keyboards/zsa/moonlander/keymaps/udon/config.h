81ac1615bc556fce227a336f801f1c452f13880d
alexanderdoroshenko <ale.doroshenko@gmail.com>
2026-03-21 19:14:22 +0100

    Add another combo for AquaVoice

--- keyboards/zsa/moonlander/keymaps/udon/config.h
+++ keyboards/zsa/moonlander/keymaps/udon/config.h
@@ -1,7 +1,7 @@
 #define USB_SUSPEND_WAKEUP_DELAY 0
-#define SERIAL_NUMBER "QmMmD/JaR0oB"
+#define SERIAL_NUMBER "QmMmD/lbJ5xa"
 #define LAYER_STATE_8BIT
-#define COMBO_COUNT 3
+#define COMBO_COUNT 4
 
 #define RGB_MATRIX_STARTUP_SPD 60
 
